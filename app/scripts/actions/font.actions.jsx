5123e860beb8ef8650e84d0973fd7e887d7f0ed6
FranzPoize <francois.poizat@gmail.com>
2017-05-16 16:42:30 +0200

    distribution tool and small fixes

--- app/scripts/actions/font.actions.jsx
+++ app/scripts/actions/font.actions.jsx
@@ -821,7 +821,7 @@ export default {
 	},
 	'/update-font': (params) => {
 		const pool = fontInstanceStore.get('fontWorkerPool');
-		const subset = 'acdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		const subset = 'Thequickfoxjmpvrtlazydg';
 		const glyph = ['b'];
 		const jobs = [];
 
@@ -840,7 +840,7 @@ export default {
 			},
 		});
 
-		const fontPromise = _.chunk(subset.split(''), Math.ceil(subset.length / pool.workerArray.length))
+		const fontPromise = _.chunk(_.uniq(subset.split('')), Math.ceil(subset.length / pool.workerArray.length))
 			.map((subsubset) => {
 				return new Promise((resolve) => {
 					jobs.push({
